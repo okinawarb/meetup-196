668249f88089367934e20a855dbdef36c1b7f0d2
tompng <tomoyapenguin@gmail.com>
2018-02-21 20:51:30 +0900

    万対応

--- kanji_to_int.rb
+++ kanji_to_int.rb
@@ -10,17 +10,25 @@ class KanjiToInt
 
   def self.ketanumber(str, keta_kanji, keta_num)
     if str.include?(keta_kanji)
-      a, str = str.split(keta_kanji)
+      a, b = str.split(keta_kanji)
       i = NUM.index(a) || 1
-      [i * keta_num, str]
+      [i * keta_num, b]
     else
       [0, str]
     end
   end
 
   def self.convert(str)
-    ans = 0
+    if str.include?('万')
+      a, b = str.split('万')
+      parse_under_man(a) * 10000 + parse_under_man(b)
+    else
+      parse_under_man(str)
+    end
+  end
 
+  def self.parse_under_man(str)
+    ans = 0
     a, str = ketanumber(str, '千', 1000)
     ans += a
     a, str = ketanumber(str, '百', 100)
@@ -39,7 +47,8 @@ class TestSample < Test::Unit::TestCase
     assert_equal 11, KanjiToInt.convert('十一')
     assert_equal 1110, KanjiToInt.convert('千百十')
     assert_equal 1111, KanjiToInt.convert('千百十一')
-    # assert_equal 2345, KanjiToInt.convert('二千三百四十五')
-    # assert_equal 12345, KanjiToInt.convert('一万二千三百四十五')
+    assert_equal 2345, KanjiToInt.convert('二千三百四十五')
+    assert_equal 12345, KanjiToInt.convert('一万二千三百四十五')
+    assert_equal 212345, KanjiToInt.convert('二十一万二千三百四十五')
   end
 end
