d333ebc05aac2d151766ae720a80e3b72c57bff8
nao taka <naopontan@gmail.com>
2018-02-21 20:11:52 +0900

    一桁に対応

--- kanji_to_int.rb
+++ kanji_to_int.rb
@@ -1,13 +1,16 @@
 require 'test/unit'
 
 class Sample
+  NUM = %w(零 一 二 三 四 五 六 七 八 九)
+
   def self.convert(str)
-    1 if str == '一'
+    NUM.index(str)
   end
 end
 
 class TestSample < Test::Unit::TestCase
   def test_one
     assert_equal 1, Sample.convert('一')
+    assert_equal 0, Sample.convert('零')
   end
 end
