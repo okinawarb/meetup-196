029a005b1d971aec57c80977b13a06b03017ffb6
nao taka <naopontan@gmail.com>
2018-02-21 20:29:45 +0900

    一万未満対応

--- kanji_to_int.rb
+++ kanji_to_int.rb
@@ -1,10 +1,35 @@
 require 'test/unit'
 
 class Sample
-  NUM = %w(零 一 二 三 四 五 六 七 八 九)
+  NUM = %w(零 一 二 三 四 五 六 七 八 九).freeze
+  LEVEL1 = %w(
+    十
+    百
+    千
+  )
 
   def self.convert(str)
-    NUM.index(str)
+    ans = 0
+    if str.include?("千")
+      a, str = str.split("千")
+      i = NUM.index(a) || 1
+      ans += i * 1000
+    end
+
+    if str.include?("百")
+      a, str = str.split("百")
+      i = NUM.index(a) || 1
+      ans += i * 100
+    end
+
+    if str.include?("十")
+      a, str = str.split("十")
+      i = NUM.index(a) || 1
+      ans += i * 10
+    end
+
+    ans += NUM.index(str) || 0
+    ans
   end
 end
 
@@ -12,5 +37,8 @@ class TestSample < Test::Unit::TestCase
   def test_one
     assert_equal 1, Sample.convert('一')
     assert_equal 0, Sample.convert('零')
+    assert_equal 11, Sample.convert('十一')
+    assert_equal 1110, Sample.convert('千百十')
+    assert_equal 1111, Sample.convert('千百十一')
   end
 end
